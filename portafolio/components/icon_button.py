9b5d5e37b9e8ce0ae4a30d13e4a8b422bb3eab3a
GCastroGarcia <Lobeno6920@gmail.com>
2026-09-15 22:53:24 -0500

    Portafolio

--- portafolio/components/icon_button.py
+++ portafolio/components/icon_button.py
@@ -2,9 +2,14 @@ import reflex as rx
 
 
 def icon_button(icon: str, url: str, text="", solid=False) -> rx.Component:
+    icon_elem = (
+        rx.box(class_name=f"devicon-{icon}-plain", font_size="1.2em")
+        if icon in ["github", "linkedin"] or "devicon" in icon
+        else rx.icon(icon)
+    )
     return rx.link(
         rx.button(
-            rx.icon(icon),
+            icon_elem,
             text,
             variant="solid" if solid else "surface"
         ),
--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,20 @@
+import os
 import reflex as rx
+
+patch_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".npm_patch.js")).replace("\\", "/")
+if os.path.exists(patch_path):
+    os.environ["NODE_OPTIONS"] = f"--require {patch_path}"
 
 config = rx.Config(
     app_name="portafolio",
+    plugins=[
+        rx.plugins.RadixThemesPlugin(
+            theme=rx.theme(
+                appearance="dark",
+                accent_color="grass",
+                radius="full",
+            )
+        ),
+        rx.plugins.SitemapPlugin(),
+    ],
 )
